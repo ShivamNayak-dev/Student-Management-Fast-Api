e9c4838e0608883fd74875e277be5f421ea72d58
Shivam Nayak <shivamkumarnayak13@gmail.com>
2026-09-03 15:51:07 +0530

    added path paramenter function

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ def home():
 @app.get("/about")
 def about():
     return {"message": "This is the Student Management API"}
+
+
+@app.get("/students/{student_id}")
+def get_student(student_id: int):
+    return {
+        "student_id": student_id
+    }
